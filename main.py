50e24be232c137373c6170a2cf3cd5af1afc91b4
jjungrrrr <datenci0987@gmail.com>
2024-04-29 15:40:05 +0900

    세번째 커밋 곱하기 완성

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def sub_func(n1,n2):
     retVal=n1-n2
     return retVal
 
+def mul_func(n1,n2):
+    retVal=n1*n2
+    return retVal
+
+
+
 ##전역변수(=클래스 변수)
 num1, num2, res = 100,200,0
-
-
 
 #메인 함수 부분
 
@@ -22,3 +26,9 @@ print(num1,'+',num2,'=',res)
 res=sub_func(num1,num2)
 
 print(num1,'-',num2,'=',res)
+
+res=mul_func(num1,num2)
+
+print(num1,'*',num2,'=',res)
+
+

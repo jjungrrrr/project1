2701508a0f4ee1144d2637fa73f17f0307dd5f68
jjungrrrr <datenci0987@gmail.com>
2024-04-29 15:36:42 +0900

    두번째 커밋 빼기 함수를 추가함

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def add_func(n1,n2):
     retVal=n1+n2
     return retVal
 
+def sub_func(n1,n2):
+    retVal=n1-n2
+    return retVal
+
 ##전역변수(=클래스 변수)
 num1, num2, res = 100,200,0
 
@@ -13,3 +17,8 @@ num1, num2, res = 100,200,0
 res=add_func(num1,num2)
 
 print(num1,'+',num2,'=',res)
+
+
+res=sub_func(num1,num2)
+
+print(num1,'-',num2,'=',res)
